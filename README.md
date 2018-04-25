56c4849fb85d27298b856a4d8073a971481820d2
ashiato45 <katiiijp@gmail.com>
2018-04-26 07:14:54 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# modineq
+You can play it here: http://ashiato45.github.io/modineq/index.html
---
+++ index.ts
@@ -0,0 +1,60 @@
+var canvas: HTMLCanvasElement;
+var ctx: CanvasRenderingContext2D | null;
+var topm = 10;
+var leftm = 10;
+var size = 500;
+
+interface IPos {
+    x: number;
+    y: number;
+}
+
+function getPos(sp: number, x_: number, y_: number): IPos {
+    return { x: leftm + sp * x_, y: size + topm - sp * y_ }
+}
+
+function draw() {
+    if (ctx == null) return;
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, 1280, 720);
+
+    var a = parseInt((<HTMLInputElement>document.getElementById("txtA")).value);
+    var b = parseInt((<HTMLInputElement>document.getElementById("txtB")).value);
+    var c = parseInt((<HTMLInputElement>document.getElementById("txtC")).value);
+    var m = parseInt((<HTMLInputElement>document.getElementById("txtM")).value);
+
+    var sp = size / m;
+    for (var i = 0; i < m; i++) {
+        for (var j = 0; j < m; j++) {
+            ctx.beginPath();
+            ctx.moveTo(leftm, size + topm - sp * j);
+            ctx.lineTo(size + leftm, size + topm - sp * j);
+            ctx.stroke();
+        }
+        ctx.beginPath();
+        ctx.moveTo(leftm + sp * i, topm);
+        ctx.lineTo(leftm + sp * i, topm + size);
+        ctx.stroke();
+
+    }
+    for (var i = 0; i < m; i++) {
+        for (var j = 0; j < m; j++) {
+            if ((a * i + b * j) % m <= c % m) {
+                ctx.beginPath();
+                var p = getPos(sp, i, j);
+                ctx.arc(p.x, p.y, sp / 3, 0, 2 * Math.PI, false);
+                ctx.fillStyle = "Red";
+                ctx.fill();
+            }
+        }
+    }
+
+}
+
+window.onload = () => {
+    canvas = <HTMLCanvasElement>document.getElementById('cnvs');
+    ctx = canvas.getContext("2d");
+    (<HTMLButtonElement>document.getElementById("btnGo")).onclick = draw;
+}
+
+
